c0de6eaadc590320791055998fa0081bea7b664c
sojeongw <sojeongw.official@gmail.com>
2025-04-27 04:29:50 +0900

    feat: 보드를 만든다

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-export default function Square() {
-  return <button className="square">X</button>;
+/**
+ * 
+ * export: 외부에서 접근 가능
+ * default: 코드를 사용하는 다른 파일에서 이 함수가 주요 함수임을 알림
+ */
+export default function Board() {
+  return (
+  <>
+  <div className="board-row">
+   <button className="square">1</button>
+   <button className="square">2</button>
+   <button className="square">3</button>
+  </div>
+  <div className="board-row">
+   <button className="square">4</button>
+   <button className="square">5</button>
+   <button className="square">6</button>
+  </div>
+  <div className="board-row">
+   <button className="square">7</button>
+   <button className="square">8</button>
+   <button className="square">9</button>
+  </div>
+  </>
+  );
 }
