06ab5afbc8fa054e8ecfebc32eefef38bb333774
sojeongw <sojeongw.official@gmail.com>
2025-04-30 15:29:53 +0900

    feat: 승자를 결정한다

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
 import { useState } from 'react';
 
+function Square({value, onSquareClick}) {
+  return (<button className="square" onClick={onSquareClick}>{value}</button>);
+}
+
 /**
  * 
  * export: 외부에서 접근 가능
  * default: 코드를 사용하는 다른 파일에서 이 함수가 주요 함수임을 알림
  */
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSqaures] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if(squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSqaures(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner; 
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
   <>
+  <div className="status">{status}</div>
   <div className="board-row">
-   <Square/>
-   <Square/>
-   <Square/>
+   <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+   <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+   <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
   </div>
   <div className="board-row">
-   <Square/>
-   <Square/>
-   <Square/>
+   <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+   <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+   <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
   </div>
   <div className="board-row">
-   <Square/>
-   <Square/>
-   <Square/>
+   <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+   <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+   <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
   </div>
   </>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState(null);
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
-  function handleClick() {
-    setValue('X');
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
-  return (<button className="square" onClick={handleClick}>{value}</button>);
+  return null;
 }
