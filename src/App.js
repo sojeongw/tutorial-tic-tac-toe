ee4a0fafa4f51f0fb5ba5f35f1e24d2034b1f83a
sojeongw <sojeongw.official@gmail.com>
2025-05-01 15:07:24 +0900

    feat: 히스토리를 저장한다

--- src/App.js
+++ src/App.js
@@ -1,59 +1,97 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function Square({value, onSquareClick}) {
-  return (<button className="square" onClick={onSquareClick}>{value}</button>);
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
-/**
- * 
- * export: 외부에서 접근 가능
- * default: 코드를 사용하는 다른 파일에서 이 함수가 주요 함수임을 알림
- */
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSqaures] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
-    if(xIsNext) {
+    if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSqaures(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = "Winner: " + winner; 
+    status = "Winner: " + winner;
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
-  <>
-  <div className="status">{status}</div>
-  <div className="board-row">
-   <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-   <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-   <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-  </div>
-  <div className="board-row">
-   <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-   <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-   <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-  </div>
-  <div className="board-row">
-   <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-   <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-   <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-  </div>
-  </>
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+/**
+ *
+ * export: 외부에서 접근 가능
+ * default: 코드를 사용하는 다른 파일에서 이 함수가 주요 함수임을 알림
+ */
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {}
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
@@ -66,12 +104,12 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
 
-  for (let i=0; i<lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
